df2d3d88db56b7ab09faa34384cb11bf2d5a175f
Lawson <wheatlel@union.edu>
2022-05-19 14:56:33 -0400

    fixes

--- frontend-node/src/Profile.js
+++ frontend-node/src/Profile.js
@@ -14,7 +14,9 @@ function Profile() {
   const [file, setFile] = useState("");
   const [upload, setUpload] = useState(false);
   const [filename, setFilename] = useState ("");
-  const [vala, setVal] = useState("");
+  const [vala, setVal] = useState(false);
+  let profile = "";
+
   function getBase64(gfile) {
     var reader = new FileReader();
     reader.readAsDataURL(gfile);
@@ -27,13 +29,19 @@ function Profile() {
       console.log('Error: ', error);
     };
  }
+
   function changeFileEvent(e){
     getBase64(e.target.files[0]);
   }
+
   function uploa() {
     setUpload(true);
   }
+
   useEffect(() => { if(upload){fetchWrapper.post("http://127.0.0.1:80/modifyppic", {"file":file,"filename": filename})} }, [file, filename, upload]);
+  useEffect(() => { fetchWrapper.get("http://127.0.0.1:80/get-profile/"+iid).then(result =>{setUserdata(result);}) }, [iid]);
+
+
   if(!iid){
       fetchWrapper.get("http://127.0.0.1:80/get-username")
       .then(result =>
@@ -41,32 +49,31 @@ function Profile() {
         console.log(result.username);
     })
   }
-  if(iid && !userData){
-    fetchWrapper.get("http://127.0.0.1:80/profile/"+iid)
-    .then(result =>
-    {setUserdata(result);})
-  }
+
   if(iid && userData && !loaded){
         fetchWrapper.get("http://127.0.0.1:80/profile/"+iid+"/feed").then(result => {
           delete result.access_token;
           finishedLoading(true);
           setItems(result);
         });
+
   } else{
     if (!id && !vala){
       setVal(
-        <form className="lf" onSubmit={uploa}>
-        <input className="bt in" type="file" name="file" onChange={e => changeFileEvent(e)}/>
-        <input className="bt in" type="submit"></input>
+        <form className="ch" onSubmit={uploa}>
+        <input className="chp" type="file" name="file" onChange={e => changeFileEvent(e)}/>
+        <input className="chp" type="submit"></input>
       </form>
       );
-      console.log("Form loaded...");
     }
+
+    console.log("Userdata");
+    console.log(userData);
     return (
         <div className="center">
             <div className="pad">
             <div className="profile">
-                <div id = "profile-pic"><img src ={"http://127.0.0.1:80/" + userData.picture}></img></div>
+                <div className = "profile-pic-div"><img className = "profile-pic" src ={"http://127.0.0.1:80/" + userData.picture}></img></div>
                 {vala}
                 <form className="lf" ></form>
                 <div id = "profile-username"></div>
@@ -88,7 +95,7 @@ function Profile() {
         </div>
         </div>
         );
-    }
+  }
 }
   
 export default Profile;
