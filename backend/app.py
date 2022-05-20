0d69fdfe630026b6616875198325697085c8747d
Lawson <wheatlel@union.edu>
2022-05-20 00:01:39 -0400

    dsaf

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ def allowed_file(filename):
 @api.route("/storage/<id>")
 def retFile(id):
     return send_from_directory('storage', id)
-@api.route("/upload", methods=["POST", "OPTIONS"])
+@api.route("/upload", methods=["POST"])
 @jwt_required()
 def upload():
     print(request.json)
